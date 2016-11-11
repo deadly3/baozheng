eef7c6de1fca7aa3173ba5fcfa6e8cdbae405fae
valery0510 <valery0510@gmail.com>
2016-11-11 22:32:01 +0800

    add admin user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,11 @@ class Admin::UsersController < ApplicationController
   before_filter :require_is_admin
   layout "admin"
 
+
+  def index
+    @users = User.all_except(current_user)
+  end
+
   def make_admin
     @user = User.find(params[:id])
     @user.is_admin = true
