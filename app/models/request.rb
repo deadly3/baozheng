c4836621b2a2f277eb96514a8759896a6b7983cb
yzha3917 <boston.zhang@gmail.com>
2016-11-12 21:21:32 +0800

    Upload successful

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :user
 
-  mount_uploader :before_picture, BeforePictureUploader
-  mount_uploader :dream_picture, DreamPictureUploader
+  mount_uploader :BeforePicture, BeforePictureUploader
+  mount_uploader :DreamPicture, DreamPictureUploader
 
 
   validates :title, presence: true
--- db/migrate/20161112094550_add_pics_to_requests.rb
+++ db/migrate/20161112094550_add_pics_to_requests.rb
@@ -2,5 +2,6 @@ class AddPicsToRequests < ActiveRecord::Migration[5.0]
   def change
     add_column :requests, :BeforePicture, :string
     add_column :requests, :DreamPicture, :string
+
   end
 end
