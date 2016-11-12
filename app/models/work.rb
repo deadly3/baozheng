ec118307feb3cf1a8f5c4cccc84077591c641d58
valery0510 <valery0510@gmail.com>
2016-11-12 17:35:30 +0800

    image uploa

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,7 @@ class Work < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price,presence: true, numericality: { greater_than: 0}
+
+  mount_uploader :image, ImageUploader
+
 end
