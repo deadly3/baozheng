b10b3be82307b4a847bacf777ce49d9e120a3ebe
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-11-13 15:55:43 +0800

    add personal details to user'

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@
 #  last_sign_in_ip        :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  is_admin               :boolean          default(FALSE)
+#  nickname               :string           default("")
+#  description            :text
+#  avatar                 :string
 #
 
 class User < ApplicationRecord
