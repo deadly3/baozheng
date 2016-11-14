c662392760f1296af03d7aa7e609224a2612762b
simon <wangzd.fudan@gmail.com>
2016-11-14 10:45:53 +0800

    在welcome页面中加入具体的切板内容，并且测试完成通过

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161113065737) do
 
-  create_table "profiles", force: :cascade do |t|
-    t.string   "nickname",   default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
