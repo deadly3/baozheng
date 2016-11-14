9fe892038fc207cc6557903f6b5390f0fdeb2c44
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-11-14 11:36:04 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161114022532) do
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "nickname",   default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -45,9 +38,8 @@ ActiveRecord::Schema.define(version: 20161114022532) do
     t.text     "description"
     t.integer  "price"
     t.string   "image"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "is_hidden",   default: true
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
   end
 
