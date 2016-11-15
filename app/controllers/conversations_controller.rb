c30cfcc2304f720298c6fc705573d26bf63767e0
ZengGIFT <oceanuszeng@gmail.com>
2016-11-16 06:55:06 +0800

    reply mail

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -11,6 +11,12 @@ class ConversationsController < ApplicationController
   def show
   end
 
+  def reply
+    current_user.reply_to_conversation(@conversation, params[:body])
+    flash[:success] = 'Reply sent'
+    redirect_to conversation_path(@conversation)
+  end
+
   private
 
     def get_mailbox
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :conversations
+  resources :conversations do
+    member do
+      post :reply
+    end
+  end
   resources :messages
 
   namespace :account do
