f0d66cbf8f2a8eef7032ab9b6f474f2428ca1206
xdite <xuite.joke@gmail.com>
2016-11-17 20:04:16 +0800

    can add conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,5 +6,22 @@ class ConversationsController < ApplicationController
     @request = Request.find_by_token(params[:request_id])
 
     @conversation = @request.conversation
+
+    @messages = @conversation.messages if @conversation.present?
+
+  end
+
+  def create
+    @request = Request.find_by_token(params[:request_id])
+    @conversation = @request.conversation
+
+    if @conversation.blank?
+      current_user.send_message(@request.user , params[:body] , @request.title, @request )
+    else
+      current_user.reply_to_conversation(@conversation, params[:body])
+    end
+
+    redirect_to request_conversations_path(@request.token)
+
   end
 end
