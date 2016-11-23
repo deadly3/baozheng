6140cd284196052bf69a1a4c3ec7725eb7069579
valery0510 <valery0510@gmail.com>
2016-11-23 21:45:05 +0800

    add nickname when register but must login with nickname too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,13 +37,4 @@ class ApplicationController < ActionController::Base
   # end
 
 
-    private
-
-    def sign_up_params
-      params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
-    end
-
-    def account_update_params
-      params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :current_password)
-    end
 end
