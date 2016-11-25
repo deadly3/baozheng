e3c1133ac6d552fd0d6ac2f30cf107d35e574af8
valery0510 <valery0510@gmail.com>
2016-11-25 22:46:30 +0800

    fix navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
   end
 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:token, :nickname)
+  end
 end
