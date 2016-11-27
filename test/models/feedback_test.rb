508a1e6b93bf3371023d39e22e10b72af03bc299
simon <wangzd.fudan@gmail.com>
2016-11-27 10:59:22 +0800

    产生feedback model并且建立链接

---
+++ test/models/feedback_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class FeedbackTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  rating     :integer
+#  commit     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
