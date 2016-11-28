f37aa01195385398bfabc779a1ab6d7c6650d12e
valery0510 <valery0510@gmail.com>
2016-11-28 13:00:08 +0800

    fix rating button

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,7 +2,7 @@ class RequestsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @requests = Request.all.request_made.recent.paginate(:page => params[:page], :per_page => 8)
+    @requests = Request.request_made.recent.paginate(:page => params[:page], :per_page => 8)
   end
 
   def new
@@ -44,6 +44,6 @@ class RequestsController < ApplicationController
   end
 
 
-  
+
 
 end
