7e60e862f1f3c6b7c5237d32d905e9d651098e22
ZengGIFT <oceanuszeng@gmail.com>
2016-11-29 11:20:02 +0800

    add_is_candidate_to_user

--- app/controllers/account/works_controller.rb
+++ app/controllers/account/works_controller.rb
@@ -5,4 +5,27 @@ class Account::WorksController < ApplicationController
   def index
     @works = current_user.works
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    @work.user = current_user
+    if @work.save
+      current_user.is_candidate = true
+      current_user.save
+      redirect_to account_user_path
+      flash[:notice] = "你已经申请成为达人了！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def work_params
+      params.require(:work).permit(:title, :description, :price, :image, :user_id, :is_hidden)
+    end
 end
