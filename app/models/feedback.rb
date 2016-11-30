875a347774d9299b99d828ff94a67533d9a9ff73
simon <wangzd.fudan@gmail.com>
2016-11-30 14:48:22 +0800

    用户支付订单评价完成后，不能再次评论

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :user
+  belongs_to :request
 
   validates :user_id, presence: true
   validates :rating, presence: true, numericality: { greater_than: 0}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130061843) do
+ActiveRecord::Schema.define(version: 20161130062019) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.integer  "user_id"
