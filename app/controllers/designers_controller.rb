91d82aa0e7a35f4c56e9a4b8f824be21cb7b1ba1
simon <wangzd.fudan@gmail.com>
2016-11-30 09:58:36 +0800

    板式调整 可以查看评论汇总

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -9,7 +9,7 @@ class DesignersController < ApplicationController
     @designer = User.find_by_token(params[:id])
     @works = @designer.works
     @feedbacks = @designer.feedbacks
-
+# @feedbacks = @designer.feedbacks.paginate(:page => params[:page], :per_page => 8)
 
   end
 end
