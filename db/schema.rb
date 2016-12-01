eb8a78a48f1eb933f201b803ac04cd50656f581d
simon <wangzd.fudan@gmail.com>
2016-12-01 16:33:34 +0800

    修改板式，同时加入搜索结果，不可以搜索为空

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161201070251) do
 
   create_table "feedbacks", force: :cascade do |t|
